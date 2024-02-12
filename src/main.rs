847af70f968e36f36825bfdeff1eaf7ce5337119
tatitle <159749900+tatitle@users.noreply.github.com>
2024-02-12 15:40:26 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use crate::mangacross::MangaCrossComic;
 
 mod mangacross;
 
-const TARGETS: [&str; 2] = ["yabai", "shiomai"];
+const TARGETS: [&str; 3] = ["ninepeaks", "yabai", "shiomai"];
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
